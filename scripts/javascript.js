7a23c3e1ffa4c53808b7843c3a7f40e564cfa1e0
diegochagas <drochachagas@gmail.com>
2019-02-18 22:41:26 -0300

    getting elements to calculate areas

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,7 +1,23 @@
-let button = document.getElementByClassName("btn-calculate");
+function calculateAreas() {
+	let shapes = ["square", "rectangle", "circle", "triangle"];
+	let side = document.getElementById('side');
+	let length = document.getElementById('length');
+	let width = document.getElementById('width');
+	let radius = document.getElementById('radius');
+	let base = document.getElementById('base');
+	let height = document.getElementById('height');
+	values_arr = getValues(side, length, width, radius, base, height);
+	getAreas (shapes, values_arr)
+		.then(areas => {
+			document.getElementById('square-area').innerHTML = areas[0];
+			document.getElementById('rectangle-area').innerHTML = areas[1];
+			document.getElementById('circle-area').innerHTML = areas[2];
+			document.getElementById('triangle-area').innerHTML = areas[3];
+		}).catch(err => {
+			console.error(`Couldn't get the areas: ${err}`);
+		});
+}
 
-button.addEventListener("click", () => {
-	let shapes = [];
-	values_arr = [];
-	getAreas (shapes, values_arr);
-});
+function getValues(...values) {
+	return values.forEach(value => (value !== undefined ? value : 0));
+}
